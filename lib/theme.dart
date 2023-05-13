ef919c37dbe6ff5aee1b9399618e95f7eded2803
ScriptAndTonic <31628952+ScriptAndTonic@users.noreply.github.com>
2023-05-13 21:14:58 +0300

    new font

--- lib/theme.dart
+++ lib/theme.dart
@@ -6,7 +6,7 @@ import 'constants.dart';
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
-    fontFamily: 'Muli',
+    fontFamily: 'Stem',
     appBarTheme: appBarTheme(),
     textTheme: textTheme(),
     inputDecorationTheme: inputDecorationTheme(),
