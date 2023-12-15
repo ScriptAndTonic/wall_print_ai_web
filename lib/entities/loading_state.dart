97ebde23b351b7e0e6888ab871b8369362a873f9
ScriptAndTonic <31628952+ScriptAndTonic@users.noreply.github.com>
2023-12-15 18:14:53 +0200

    update build

--- lib/entities/loading_state.dart
+++
@@ -1,1 +0,0 @@
-enum LoadingState { open, loading, complete }
--- lib/entities/room_image_upload_info.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:typed_data';
-
-class RoomImageUploadInfo {
-  static String lastestUploadedRoomImageId = '';
-  static Uint8List latestRoomImageBytes = Uint8List(0);
-}
